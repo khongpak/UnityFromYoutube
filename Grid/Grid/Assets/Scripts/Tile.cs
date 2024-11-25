05594ff45c3ae13fd84fbf709d98a0fe09471043
KhongpakHomePC <afexim5@gmail.com>
2024-11-25 14:43:25 +0700

    Create Grid for 2D

---
+++ Grid/Grid/Assets/Scripts/Tile.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Color baseColor, offsetColor;
+    [SerializeField] private SpriteRenderer renderer;
+
+    public void Init(bool isOffset)
+    {
+        renderer.color = isOffset ? offsetColor : baseColor;
+    }
+}
